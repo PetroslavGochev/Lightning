1dab667e412736b7c1078a50739fc05981e051e9
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2020-12-21 12:33:57 +0200

    Created CRUD operations for requests and validations and some styling on homepage

--- Lightning/Lightning/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Lightning/Lightning/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -11,19 +11,20 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Lightning.Models.DBModels;
 
 namespace Lightning.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class LoginModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<LightningUser> _userManager;
+        private readonly SignInManager<LightningUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<IdentityUser> signInManager, 
+        public LoginModel(SignInManager<LightningUser> signInManager, 
             ILogger<LoginModel> logger,
-            UserManager<IdentityUser> userManager)
+            UserManager<LightningUser> userManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
---
+++ Lightning/Lightning/Controllers/RequestsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Lightning.Data;
+using Lightning.Models.DBModels;
+using Microsoft.AspNetCore.Identity;
+
+namespace Lightning.Controllers
+{
+    public class RequestsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<LightningUser> _userManager;
+
+        public RequestsController(ApplicationDbContext context, UserManager<LightningUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Requests
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return this.Redirect("/");
+            }
+            else
+            {
+                return View(await _context.Requests.Where(x => x.LightningUser.Id == user.Id).ToListAsync());
+            }
+        }
+
+        // GET: Requests/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var request = await _context.Requests
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (request == null)
+            {
+                return NotFound();
+            }
+
+            return View(request);
+        }
+
+        // GET: Requests/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Requests/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Status,Content,DateFrom,DateTo,Package,Address")] Request request)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (ModelState.IsValid)
+            {
+                var requestToAdd = request.LightningUser = user;
+                _context.Add(request);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(request);
+        }
+
+        // GET: Requests/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var request = await _context.Requests.FindAsync(id);
+            if (request == null)
+            {
+                return NotFound();
+            }
+            return View(request);
+        }
+
+        // POST: Requests/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Status,Content,DateFrom,DateTo,Package,Address")] Request request)
+        {
+            if (id != request.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(request);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RequestExists(request.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(request);
+        }
+
+        // GET: Requests/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var request = await _context.Requests
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (request == null)
+            {
+                return NotFound();
+            }
+
+            return View(request);
+        }
+
+        // POST: Requests/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var request = await _context.Requests.FindAsync(id);
+            _context.Requests.Remove(request);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RequestExists(int id)
+        {
+            return _context.Requests.Any(e => e.Id == id);
+        }
+    }
+}
